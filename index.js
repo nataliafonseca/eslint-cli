25dcfb4cf3b9ff67fbfe58bb507492d403a15532
Natália Fonseca <nataliabf@outlook.com>
2022-08-20 21:53:30 -0300

    fix: fix skiping eslint configs

--- index.js
+++ index.js
@@ -1,33 +1,18 @@
 #! /usr/bin/env node
 
-import fs from "fs";
+import fs from 'fs';
 
-import inquirer from "inquirer";
-import { createSpinner } from "nanospinner";
-import exec from "node-async-exec";
+import inquirer from 'inquirer';
+import { createSpinner } from 'nanospinner';
+import exec from 'node-async-exec';
 
-import { prettierConfig } from "./configs/prettier.js";
-import { reactJsEslintConfig } from "./configs/react-js.js";
-import { reactTsEslintConfig } from "./configs/react-ts.js";
-
-const answers = await inquirer.prompt([
-  {
-    name: "projectType",
-    type: "list",
-    message: "What type of project are you setting up?",
-    choices: ["React - TypeScript", "React - JavaScript"],
-  },
-  {
-    name: "packageManager",
-    type: "list",
-    message: "What package manager are you using?",
-    choices: ["Yarn", "npm"],
-  },
-]);
+import { prettierConfig } from './configs/prettier.js';
+import { reactJsEslintConfig } from './configs/react-js.js';
+import { reactTsEslintConfig } from './configs/react-ts.js';
 
 async function install(packageName) {
   try {
-    if (answers.packageManager === "Yarn") {
+    if (answers.packageManager === 'Yarn') {
       await exec({ cmd: `yarn add -D ${packageName}` });
     } else {
       await exec({ cmd: `npm i -D ${packageName}` });
@@ -37,46 +22,40 @@ async function install(packageName) {
   }
 }
 
-async function handleDependenciesInstallation() {
-  const spinner = createSpinner("Installing required dependencies...").start();
+const answers = await inquirer.prompt([
+  {
+    name: 'projectType',
+    type: 'list',
+    message: 'What type of project are you setting up?',
+    choices: ['React - TypeScript', 'React - JavaScript'],
+  },
+  {
+    name: 'packageManager',
+    type: 'list',
+    message: 'What package manager are you using?',
+    choices: ['Yarn', 'npm'],
+  },
+]);
 
-  await install("eslint");
-  await install("prettier");
-  await install("eslint-config-prettier");
-  await install("eslint-plugin-prettier");
+console.log();
 
-  if (answers.projectType === "React - TypeScript") {
-    await install("typescript");
-    await install("@typescript-eslint/eslint-plugin");
-    await install("@typescript-eslint/parser");
-    await install("eslint-plugin-react");
-    await install("eslint-plugin-react-hooks");
-    await install("eslint-plugin-react-perf");
-  }
+const spinner = createSpinner('Installing required dependencies...').start();
 
-  if (answers.projectType === "React - JavaScript") {
-    await install("eslint-plugin-react");
-    await install("eslint-plugin-react-hooks");
-    await install("eslint-plugin-react-perf");
-  }
+fs.writeFileSync('.prettierrc.json', JSON.stringify(prettierConfig, null, 2));
 
-  spinner.success("Projeto configurado com sucesso!");
-}
+if (answers.projectType === 'React - TypeScript') {
+  fs.writeFileSync('.eslintrc.json', JSON.stringify(reactTsEslintConfig, null, 2));
 
-fs.writeFileSync(".prettierrc.json", JSON.stringify(prettierConfig, null, 2));
-
-if (answers.projectType === "React - TypeScript" && answers.typescript) {
-  fs.writeFileSync(
-    ".eslintrc.json",
-    JSON.stringify(reactTsEslintConfig, null, 2)
+  await install(
+    'eslint prettier eslint-config-prettier eslint-plugin-prettier typescript @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint-plugin-react eslint-plugin-react-hooks eslint-plugin-react-perf',
   );
 }
 
-if (answers.projectType === "React - JavaScript" && answers.typescript) {
-  fs.writeFileSync(
-    ".eslintrc.json",
-    JSON.stringify(reactJsEslintConfig, null, 2)
+if (answers.projectType === 'React - JavaScript') {
+  fs.writeFileSync('.eslintrc.json', JSON.stringify(reactJsEslintConfig, null, 2));
+  await install(
+    'eslint prettier eslint-config-prettier eslint-plugin-prettier eslint-plugin-react eslint-plugin-react-hooks eslint-plugin-react-perf',
   );
 }
 
-handleDependenciesInstallation();
+spinner.success({ text: 'Projeto configurado com sucesso!' });
