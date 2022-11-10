449b638bb6e52b0c5f070f293cb5881fd793ffcb
Natália Fonseca <natalia@nataliafonseca.dev>
2022-11-09 23:30:10 -0300

    Revert "feat: trying new things"
    This reverts commit e3ff609094c2bb180b3fe6c12924c067cf0493c0.

--- configs/react-ts.js
+++ configs/react-ts.js
@@ -15,10 +15,6 @@ export const reactTsEslintConfig = {
     "plugin:react/recommended",
     "plugin:react-hooks/recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
     "prettier",
     "plugin:prettier/recommended",
   ],
@@ -26,54 +22,12 @@ export const reactTsEslintConfig = {
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
-      modules: true,
     },
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: [
-    "simple-import-sort",
-    "import",
-    "react",
-    "react-hooks",
-    "@typescript-eslint",
-    "prettier",
-  ],
-  ignorePatterns: ["jest.config.js", "webpack.config.js", "setupTests.js"],
+  plugins: ["react", "react-hooks", "@typescript-eslint", "prettier"],
   rules: {
-    "import/extensions": [
-      0,
-      "always",
-      { js: "never", ts: "never", jsx: "never", tsx: "never" },
-    ],
-    "comma-dangle": [
-      "error",
-      {
-        arrays: "always-multiline",
-        objects: "always-multiline",
-        imports: "always-multiline",
-        exports: "always-multiline",
-        functions: "never",
-      },
-    ],
-    "no-restricted-imports": [
-      "error",
-      {
-        patterns: [
-          {
-            group: ["**/src/*"],
-            message:
-              "Relative imports outside the /src folder will not function once published. Please remain inside the /src folder instead.",
-          },
-        ],
-      },
-    ],
-    "react/no-unstable-nested-components": [
-      "error",
-      {
-        allowAsProps: false,
-      },
-    ],
     "prettier/prettier": [
       "error",
       {
@@ -87,7 +41,4 @@ export const reactTsEslintConfig = {
     "react/prop-types": "off",
     "react-hooks/rules-of-hooks": "error",
   },
-
-  "import/no-cycle": "error",
-  "import/no-unresolved": "error",
 };
